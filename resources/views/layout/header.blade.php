980b202fcfc39866680962888cf46485d76ed31e
Ankit Meshram <ankit@brokod.com>
2022-08-14 23:20:42 +0530

    lp-2 - hero section created

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Lead Pest - Pest control management system</title>
 
     <style>
       * {
@@ -16,6 +16,29 @@
           margin: 0;
           font-family: sans-serif;
       }
+      h1 {
+        font-size: 4em;
+        font-weight: 700;
+      }
+      p {
+        font-size: 1.1rem;
+        font-weight: 600;
+        color: rgba(0, 0, 0, 0.705);
+      }
+      .btn-orange {
+        background: orangered;
+        color: white;
+        font-size: 18px;
+        padding: 10px 20px;
+      }
+      @media(max-width: 576px) {
+        h1 {
+        font-size: 3em;
+      }
+      .mb-pt-20 {
+        margin-top: 50px;
+      }
+    }
       .navbar {
         background: orangered;
         padding: 20px;
@@ -30,6 +53,9 @@
       }
       .navbar .nav-item .dropdown-menu:hover {
         background: rgba(255, 68, 0, 0.849)
+      }
+      .hero {
+        padding: 100px 10px;
       }
     </style>
   </head>
---
+++ resources/views/layout/hero.blade.php
@@ -0,0 +1,15 @@
+<div class="container hero">
+    <div class="row">
+        <div class="col-md-6 my-auto">
+            <h1 class="pb-2">Customize your Experience</h1>
+            <p  class="pb-2">Samu lets you manage software development and track bugs. Samu streamlined design is built for speed.</p>
+            <button class="btn btn-orange">Let's Start</button>
+        
+        </div>
+        <div class="col-md-6 mb-pt-20">
+            <div class="text-center">
+                <img src="https://themeforest.kreativdev.com/samu/demo/assets/images/banner/banner-5.png" loading="lazy" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</div>
